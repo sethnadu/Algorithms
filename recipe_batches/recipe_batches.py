3cae94f0c82da636428001a92a96c9abc70f2be3
Seth Nadu <seth.nadu@gmail.com>
2019-12-11 16:37:09 -0500

    finished recipe_batches

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -3,12 +3,24 @@
 import math
 
 def recipe_batches(recipe, ingredients):
-  pass 
+  totals = []
+  if len(recipe) is not len(ingredients):
+    return 0
+  for i in recipe:
+    if i in ingredients:
+      totals.append(ingredients[i] // recipe[i])
+      print(" totals :", totals)
+    else:
+      totals.append(0)
+  return min(totals)
 
 
-if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
+ingredients = { 'milk': 132, 'butter': 51, 'flour': 51 }
+recipe_batches(recipe, ingredients)
+# if __name__ == '__main__':
+#   # Change the entries of these dictionaries to test 
+#   # your implementation with different inputs
+#   recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
+#   ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
+#   print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -3,7 +3,7 @@
 import argparse
 import time
 def find_max_profit(prices):
-  firstTime = time.time()
+  # firstTime = time.time()
   largest_margin = int()
   margin = int()
   for i in range(len(prices)):
@@ -14,8 +14,8 @@ def find_max_profit(prices):
       else: 
         margin = diff
   # print("Largest Margin: ", largest_margin)
-  endTime = time.time()
-  print("TIme: ", endTime - firstTime, " seconds")
+  # endTime = time.time()
+  # print("TIme: ", endTime - firstTime, " seconds")
   if largest_margin == 0:
     print("Profit :", margin)
     return margin
@@ -26,10 +26,10 @@ def find_max_profit(prices):
 # find_max_profit([1050, 270, 1540, 3800, 2])
 # find_max_profit([10, 7, 5, 8, 11, 9])
 
-if __name__ == '__main__':
-  # This is just some code to accept inputs from the command line
-  parser = argparse.ArgumentParser(description='Find max profit from prices.')
-  parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer price')
-  args = parser.parse_args()
+# if __name__ == '__main__':
+#   # This is just some code to accept inputs from the command line
+#   parser = argparse.ArgumentParser(description='Find max profit from prices.')
+#   parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer price')
+#   args = parser.parse_args()
 
-  print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
+#   print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
